ae834e05186857b6b85cbfb9ffc8d320c0e46efd
Matias Crespi <mat.cres@gmail.com>
2019-07-10 16:38:33 +0200

    chore: wip remove express-winston, integrate morgan+winston

--- config/express.js
+++ config/express.js
@@ -6,21 +6,24 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
 import expressValidation from 'express-validation'
-import expressWinston from 'express-winston'
+// import expressWinston from 'express-winston'
 import helmet from 'helmet'
 import httpStatus from 'http-status'
-import logger from 'morgan'
+import morgan from 'morgan'
 import methodOverride from 'method-override'
 import routes from '../server/routes'
-import winstonInstance from './winston'
-import oAuthComponents        from '../server/controllers/oauth'
+// import winstonInstance from './winston'
+import winston from './winston'
+import oAuthComponents from '../server/controllers/oauth'
 
 
 const app = express()
 
-if (config.env === 'development') {
-  app.use(logger('dev'))
-}
+// if (config.env === 'development') {
+//   app.use(morgan('dev'))
+// }
+// app.use(morgan('dev'))
+app.use(morgan('combined', { stream: winston.stream, }))
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
@@ -40,16 +43,16 @@ app.disable('x-powered-by')
 app.use(cors())
 
 // enable detailed API logging in dev env
-if (config.env === 'development') {
-  expressWinston.requestWhitelist.push('body')
-  expressWinston.responseWhitelist.push('body')
-  app.use(expressWinston.logger({
-    winstonInstance,
-    meta: true,   // optional: log meta data about request (defaults to true)
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    colorStatus: true,   // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
-  }))
-}
+// if (config.env === 'development') {
+//   expressWinston.requestWhitelist.push('body')
+//   expressWinston.responseWhitelist.push('body')
+//   app.use(expressWinston.logger({
+//     winstonInstance,
+//     meta: true,   // optional: log meta data about request (defaults to true)
+//     msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+//     colorStatus: true,   // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+//   }))
+// }
 
 // mount all routes on /api path
 app.use('/', routes)
@@ -78,11 +81,11 @@ app.use((req, res, next) => {
 })
 
 // log error in winston transports except when executing test suite
-if (config.env !== 'test') {
-  app.use(expressWinston.errorLogger({
-    winstonInstance,
-  }))
-}
+// if (config.env !== 'test') {
+//   app.use(expressWinston.errorLogger({
+//     winstonInstance,
+//   }))
+// }
 
 // error handler, send stacktrace only during development
 app.use((err, req, res, next) => {
--- config/winston.js
+++ config/winston.js
@@ -1,3 +1,56 @@
+import winston from 'winston'
+import config from './env'
+
+// TODO: WIP...
+const options = {
+  file: {
+    level: config.logger.level,
+    filename: config.logger.logFile,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+    format: winston.format.printf(({ timestamp, level, message, }) => {
+      return `${timestamp} ${level} message: ${message}`
+    }),
+  },
+  console: {
+    level: config.logger.level,
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+    format: winston.format.printf(({ timestamp, level, message, }) => {
+      return `${timestamp} ${level} message: ${message}`
+    }),
+  },
+}
+
+const logger = new winston.createLogger({
+    format: winston.format.combine(
+      winston.format.splat(),
+      winston.format.simple(),
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.prettyPrint(),
+      winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console),
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+})
+
+logger.stream = {
+  write: (message, encoding) => {
+    logger.info(message)
+  },
+}
+
+export default logger
+
+// Example Code For Winston 2
 // import winston from 'winston'
 // import config from './env'
 
@@ -17,36 +70,36 @@
 // })
 
 
-// Code For Winston 3
+// Example Code For Winston 3
 // Ref: https://github.com/winstonjs/winston/blob/master/README.md
 // Ref: https://github.com/winstonjs/winston/issues/1336
 
-import { format, transports, createLogger, config, } from 'winston'
+// import { format, transports, createLogger, config, } from 'winston'
 
-export default createLogger({
-  // defaultMeta: { service: 'user-service', },
-  level: 'info',
-  levels: config.syslog.levels,
-  format: format.combine(
-    format.splat(),
-    format.simple(),
-    format.colorize(),
-    format.timestamp(),
-    format.prettyPrint(),
-    format.json(),
-    format.printf((info) => {
-      return Object.keys(info).reverse().reduce((acc, key, i) => {
-        if (typeof key === 'string') {
-          if (i > 0) acc += ', '
-          acc += `'${key}': '${info[key]}'`
-        }
-        return acc
-      }, '{ ') + ' }'
-    })
-  ),
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: 'error.log', level: 'error', }),
-    new transports.File({ filename: 'combined.log', }),
-  ],
-})
+// export default createLogger({
+//   // defaultMeta: { service: 'user-service', },
+//   level: 'info',
+//   levels: config.syslog.levels,
+//   format: format.combine(
+//     format.splat(),
+//     format.simple(),
+//     format.colorize(),
+//     format.timestamp(),
+//     format.prettyPrint(),
+//     format.json(),
+//     format.printf((info) => {
+//       return Object.keys(info).reverse().reduce((acc, key, i) => {
+//         if (typeof key === 'string') {
+//           if (i > 0) acc += ', '
+//           acc += `'${key}': '${info[key]}'`
+//         }
+//         return acc
+//       }, '{ ') + ' }'
+//     })
+//   ),
+//   transports: [
+//     new transports.Console(),
+//     new transports.File({ filename: 'error.log', level: 'error', }),
+//     new transports.File({ filename: 'combined.log', }),
+//   ],
+// })
